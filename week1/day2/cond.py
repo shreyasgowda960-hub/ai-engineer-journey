4fb18e684012aca8fa63eef8f792ab6b340ac1e9
Shrey <shreyasgowda960@gmail.com>
2026-07-24 13:50:44 +0530

    Day 2: conditionals and loops

---
+++ week1/day2/cond.py
@@ -0,0 +1,19 @@
+#if elif else
+
+x=int(input("Enter a number check conditionality "))
+if x== 10:
+    print("it's 10")
+elif x<=14:
+    print("less than intended")
+else :
+    print("Unknown")
+
+#for loop 
+ 
+for y in range(7):
+    print(y)
+
+for i in range(1,40,15):
+    print(i)
+
+    
---
+++ week1/day2/day2_task.py
@@ -0,0 +1,15 @@
+x=int(input("Enter Your Grade "))
+
+if x>100 or x<0:
+    print("wrong entry")
+elif x<40:
+    print("Fail")
+elif x>=90:
+    print("A grade")
+elif x>=75:
+    print("B grade")
+elif x>=60:
+    print("C grade")    
+elif x>=40 :
+    print("D grade")
+   
